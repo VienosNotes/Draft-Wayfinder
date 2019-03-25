27f95991df4e3d61e4842da1d949c4c425160818
D.Aoki <vienosnotes@gmail.com>
2019-03-25 15:55:27 +0900

    init

---
+++ Controls/GlowWindow.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shell;
+using Livet.Commands;
+
+namespace DraftWayfinder.Controls
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GlowWindow : Window
+    {
+        static GlowWindow()
+        {
+            ActiveTitleCaptionBrush = Brushes.White;
+            InactiveTitleCaptionBrush = Brushes.LightGray;
+            TitleCaptionBrushProperty =
+                DependencyProperty.Register("TitleCaptionBrush", typeof(Brush), typeof(GlowWindow), new PropertyMetadata(InactiveTitleCaptionBrush));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GlowWindow), new FrameworkPropertyMetadata(typeof(GlowWindow)));
+        }
+
+        public GlowWindow()
+        {
+            WindowChrome.SetWindowChrome(this, new WindowChrome
+            {
+                CaptionHeight = SystemParameters.CaptionHeight,
+                ResizeBorderThickness = SystemParameters.WindowResizeBorderThickness,
+                GlassFrameThickness = new Thickness(0),
+                CornerRadius = new CornerRadius(0)
+            });
+
+            Activated += OnActivated;
+            Deactivated += OnDeactivated;
+        }
+
+        #region Brushes
+        /// <summary>
+        /// アクティブ時のウィンドウボーダーのブラシ（デフォルト色）。
+        /// </summary>
+        protected static readonly Brush ActiveBorderBrush = new SolidColorBrush(Color.FromArgb(0x66, 0xFF, 0xFF, 0xFF));
+        /// <summary>
+        /// 非アクティブ時のウィンドウボーダーのブラシ。
+        /// </summary>
+        protected static readonly Brush InactiveBorderBrush = new SolidColorBrush(Color.FromArgb(255, 20, 20, 20));
+        /// <summary>
+        /// アクティブ時のタイトル文字色のブラシ。
+        /// </summary>
+        protected static readonly Brush ActiveTitleCaptionBrush;
+        /// <summary>
+        /// 非アクティブ時のタイトル文字色のブラシ。
+        /// </summary>
+        protected static readonly Brush InactiveTitleCaptionBrush;
+        #endregion
+
+        #region Dependecy Properties
+
+        #region ThemeColor
+        /// <summary>
+        /// この GlowWindow のテーマカラーを設定または取得します。
+        /// </summary>
+        public Color ThemeColor
+        {
+            get => (Color)GetValue(ThemeColorProperty);
+            set => SetValue(ThemeColorProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for ThemeColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ThemeColorProperty =
+            DependencyProperty.Register("ThemeColor", typeof(Color), typeof(GlowWindow), new PropertyMetadata(Colors.Transparent, ApplyNewThemeColor));
+
+        private static void ApplyNewThemeColor(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var gw = (GlowWindow)dependencyObject;
+            gw.TitleBarBrush = gw.CreateTitleBarBrush((Color)dependencyPropertyChangedEventArgs.NewValue);
+
+            var borderBrush = gw.CreateBorderBrush((Color)dependencyPropertyChangedEventArgs.NewValue);
+            if (gw.IsActive)
+            {
+                gw.WindowBorderBrush = borderBrush;
+            }
+
+            gw._activeBrushCache = gw.CreateBorderBrush((Color)dependencyPropertyChangedEventArgs.NewValue);
+        }
+        #endregion
+
+        #region TitleBarBrush
+        public Brush TitleBarBrush
+        {
+            get => (Brush)GetValue(TitleBarBrushProperty);
+            private set => SetValue(_titleBarBrushPropertyKey, value);
+        }
+
+        // Using a DependencyProperty as the backing store for TitleBarBrush.  This enables animation, styling, binding, etc...
+        private static readonly DependencyPropertyKey _titleBarBrushPropertyKey =
+            DependencyProperty.RegisterReadOnly("TitleBarBrush", typeof(Brush), typeof(GlowWindow), null);
+
+        public static readonly DependencyProperty TitleBarBrushProperty = _titleBarBrushPropertyKey.DependencyProperty;
+
+        protected virtual Brush CreateTitleBarBrush(Color color)
+        {
+            // 透明色の場合は全体透明で返す
+            if (color.A == 0)
+            {
+                return Brushes.Transparent;
+            }
+
+            // そうでないならグラデーションブラシに変換して返す
+            var brush = new LinearGradientBrush
+            {
+                StartPoint = new Point(0, 0.5),
+                EndPoint = new Point(1, 0.5),
+            };
+
+            brush.GradientStops.Add(new GradientStop(color, 0));
+            brush.GradientStops.Add(new GradientStop(Color.FromArgb(0x66, color.R, color.G, color.B), 0.7));
+            brush.GradientStops.Add(new GradientStop(Color.FromArgb(0x66, color.R, color.G, color.B), 1));
+
+            return brush;
+        }
+        #endregion
+
+        #region BorderBrush
+
+        private Brush _activeBrushCache;
+
+        public Brush WindowBorderBrush
+        {
+            get => (Brush)GetValue(WindowBorderBrushProperty);
+            private set => SetValue(_windowBorderBrushPropertyKey, value);
+        }
+
+        // Using a DependencyProperty as the backing store for BorderBrush.  This enables animation, styling, binding, etc...
+        private static readonly DependencyPropertyKey _windowBorderBrushPropertyKey =
+            DependencyProperty.RegisterReadOnly("WindowBorderBrush", typeof(Brush), typeof(GlowWindow), null);
+
+        public static DependencyProperty WindowBorderBrushProperty = _windowBorderBrushPropertyKey.DependencyProperty;
+
+        protected virtual Brush CreateBorderBrush(Color color)
+        {
+            return color.A == 0 ? ActiveBorderBrush : new SolidColorBrush(color);
+        }
+        #endregion
+
+        #region TitleCaptionBrush
+
+
+        public Brush TitleCaptionBrush
+        {
+            get => (Brush)GetValue(TitleCaptionBrushProperty);
+            set => SetValue(TitleCaptionBrushProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for TitleCaptionBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleCaptionBrushProperty;
+        #endregion
+        #endregion
+
+        #region Commands
+
+        #region Minimize Button
+        private ICommand _minimizeCommand;
+        public ICommand MinimizeCommand => _minimizeCommand ?? (_minimizeCommand = new ViewModelCommand(MinimizeCommandImpl));
+
+        private void MinimizeCommandImpl()
+        {
+            WindowState = WindowState.Minimized;
+        }
+        #endregion
+
+        #region Maximize Button
+        private ICommand _maximizeCommand;
+        public ICommand MaximizeCommand => _maximizeCommand ?? (_maximizeCommand = new ViewModelCommand(MaximizeCommandImpl));
+
+        private void MaximizeCommandImpl()
+        {
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+        #endregion
+
+        #region Maximize Button
+        private ICommand _closeCommand;
+        public ICommand CloseCommand => _closeCommand ?? (_closeCommand = new ViewModelCommand(Close));
+
+        protected new virtual void Close()
+        {
+            base.Close();
+        }
+        #endregion
+
+        #region アクティブ状態
+        /// <summary>
+        /// 非アクティブ状態になったときの色変え
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private void OnDeactivated(object sender, EventArgs eventArgs)
+        {
+            _activeBrushCache = WindowBorderBrush;
+            WindowBorderBrush = InactiveBorderBrush;
+            TitleCaptionBrush = InactiveTitleCaptionBrush;
+        }
+
+        /// <summary>
+        /// アクティブ状態になったときの色変え   
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private void OnActivated(object sender, EventArgs eventArgs)
+        {
+            WindowBorderBrush = _activeBrushCache;
+            TitleCaptionBrush = ActiveTitleCaptionBrush;
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Media;
+using Livet;
+
+namespace DraftWayfinder
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        #region UI string resources
+        private static readonly string _cmc = "CMC";
+        private static readonly string _power = "Power";
+        private static readonly string _toughness = "Toughness";
+        private static readonly string _numOfCreatures = "Number of Creatures";
+        private static readonly string _numOfFlyers = "Number of Flyers";
+        private static readonly string _avgPower = "Average Power";
+        private static readonly string _avgToughness = "Average Toughness";
+        private static readonly string _maxPower = "Max Power";
+        private static readonly string _maxToughness = "Max Toughness";
+        private static readonly string _avgRatio = "Average Mana Ratio";
+        private static readonly string _avgCMC = "Average CMC";
+        private static readonly string _minCMC = "Min CMC";
+
+        private static readonly IReadOnlyCollection<string> _cmcItems = new List<string>
+        {
+            _numOfCreatures, _numOfFlyers, _avgPower, _avgToughness, _maxPower, _maxToughness, _avgRatio
+        };
+
+        private static readonly IReadOnlyCollection<string> _powerItems = new List<string>
+        {
+            _numOfCreatures, _numOfFlyers, _avgCMC, _minCMC, _avgToughness, _maxToughness
+        };
+
+        private static readonly IReadOnlyCollection<string> _toughnessItems = new List<string>
+        {
+            _numOfCreatures, _numOfFlyers, _avgCMC, _minCMC, _avgPower, _maxPower
+        };
+
+        #endregion
+
+        #region Binding Properties
+
+
+        private readonly IReadOnlyCollection<string> _xAxisItems = new ReadOnlyCollection<string>(new List<string>{ _cmc, _power, _toughness });
+        public IReadOnlyCollection<string> XAxisItems => _xAxisItems;
+
+        private string _xAxis = _cmc;
+        public string XAxis
+        {
+            get => _xAxis;
+            set
+            {
+                if (_xAxis == value) { return; }
+                _xAxis = value;
+                RaisePropertyChanged();
+
+                if (value == _cmc)
+                {
+                    YAxisItems = _cmcItems;
+                    YAxis = _cmcItems.First();
+                }
+                else if (value == _power)
+                {
+                    YAxisItems = _powerItems;
+                    YAxis = _powerItems.First();
+                }
+                else if (value == _toughness)
+                {
+                    YAxisItems = _toughnessItems;
+                    YAxis = _toughnessItems.First();
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+
+                RaisePropertyChanged(nameof(YAxisItems));
+                RaisePropertyChanged(nameof(YAxis));
+            }
+        }
+
+        private IReadOnlyCollection<string> _yAxisItems = _cmcItems;
+        public IReadOnlyCollection<string> YAxisItems
+        {
+            get => _yAxisItems;
+            private set
+            {
+                if (_yAxisItems == value) { return; }
+                _yAxisItems = value;
+            }
+        }
+
+        private string _yAxis = _cmcItems.First();
+        public string YAxis
+        {
+            get => _yAxis;
+            set
+            {
+                if (_yAxis == value) { return; }
+                _yAxis = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+    }
+}
